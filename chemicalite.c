0bc305cdb4d395f02ea4199a95345276020188b5
Riccardo Vianello <riccardo.vianello@gmail.com>
2011-03-15 23:51:50 +0100

    stub (empty, do-nothing, sqlite extension)

---
+++ chemicalite.c
@@ -0,0 +1,36 @@
+#include <assert.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+
+#include <sqlite3ext.h>
+SQLITE_EXTENSION_INIT1
+
+/*
+** Register the chemicalite module with database handle db.
+*/
+int sqlite3_chemicalite_init(sqlite3 *db)
+{
+  int rc = SQLITE_OK;
+  
+  /*
+  if (rc == SQLITE_OK) {
+    rc = sqlite3_create_function(db, [...]);
+  }
+  */
+
+  /*
+  if (rc == SQLITE_OK) {
+    rc = sqlite3_create_module_v2(db, [...]);
+  }
+  */
+
+  return rc;
+}
+
+int sqlite3_extension_init(sqlite3 *db, char **pzErrMsg,
+			   const sqlite3_api_routines *pApi)
+{
+  SQLITE_EXTENSION_INIT2(pApi)
+  return sqlite3_chemicalite_init(db);
+}
