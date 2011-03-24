f3f70737468e236d530eae65d419c0b67771ac15
Riccardo Vianello <riccardo.vianello@gmail.com>
2011-03-24 08:39:26 +0100

    cosmetic

--- rdkit_adapter.h
+++ rdkit_adapter.h
@@ -7,21 +7,21 @@ extern "C" {
 
   typedef struct CMol CMol;
 
-  void free_cmol(CMol *);
+  void free_cmol(CMol *pCMol);
 
   int txt_to_cmol(const char * txt, bool as_smarts, CMol **ppCMol);
 
-  double cmol_amw(CMol *);
-  double cmol_logp(CMol *);
-  double cmol_tpsa(CMol *);
+  double cmol_amw(CMol *pCMol);
+  double cmol_logp(CMol *pCMol);
+  double cmol_tpsa(CMol *pCMol);
   
-  int cmol_hba(CMol *);
-  int cmol_hbd(CMol *);
-  int cmol_num_atms(CMol *);
-  int cmol_num_hvyatms(CMol *);
-  int cmol_num_rotatable_bnds(CMol *);
-  int cmol_num_hetatms(CMol *);
-  int cmol_num_rings(CMol *);
+  int cmol_hba(CMol *pCMol);
+  int cmol_hbd(CMol *pCMol);
+  int cmol_num_atms(CMol *pCMol);
+  int cmol_num_hvyatms(CMol *pCMol);
+  int cmol_num_rotatable_bnds(CMol *pCMol);
+  int cmol_num_hetatms(CMol *pCMol);
+  int cmol_num_rings(CMol *pCMol);
 
 #ifdef __cplusplus
 } /* extern "C" { */
