66f02542822f65a736d23bfa920dd8755a3f1998
Riccardo Vianello <riccardo.vianello@gmail.com>
2011-03-24 23:09:30 +0100

    some more (CMol <-> SMILES/SMARTS <-> blob) interconversion routines.

--- rdkit_adapter.cpp
+++ rdkit_adapter.cpp
@@ -4,18 +4,29 @@
 extern const sqlite3_api_routines *sqlite3_api;
 
 #include <cassert>
+#include <cstring>
 #include <string>
 
 #include <GraphMol/RDKitBase.h>
+#include <GraphMol/MolPickler.h>
 #include <GraphMol/Descriptors/MolDescriptors.h>
 #include <GraphMol/SmilesParse/SmilesParse.h>
+#include <GraphMol/SmilesParse/SmartsWrite.h>
+#include <GraphMol/SmilesParse/SmilesWrite.h>
 
-struct CMol : RDKit::ROMol {};
+struct CMol : RDKit::ROMol {
+  CMol() : RDKit::ROMol() {}
+  CMol(const CMol & other) : RDKit::ROMol(other) {}
+  CMol(const RDKit::ROMol & other) : RDKit::ROMol(other) {}
+  CMol(const std::string & pickle) : RDKit::ROMol(pickle) {}
+};
 
 void free_cmolecule(CMol *pCMol)
 {
   delete static_cast<RDKit::ROMol *>(pCMol);
 }
+
+// SMILES/SMARTS <-> Molecule ////////////////////////////////////////////////
 
 int txt_to_cmol(const char * txt, bool as_smarts, CMol **ppCMol)
 {
@@ -29,7 +40,8 @@ int txt_to_cmol(const char * txt, bool as_smarts, CMol **ppCMol)
     RDKit::ROMol *pROMol = as_smarts ?
       RDKit::SmartsToMol(data) : RDKit::SmilesToMol(data);
       *ppCMol = static_cast<CMol *>(pROMol);
-  } catch (...) {
+  } 
+  catch (...) {
     // problem generating molecule from smiles
     rc = SQLITE_ERROR;
   }
@@ -40,6 +52,93 @@ int txt_to_cmol(const char * txt, bool as_smarts, CMol **ppCMol)
 
   return rc;
 }
+
+int cmol_to_txt(CMol *pCMol, bool as_smarts, char **pTxt)
+{
+  assert(pCMol);
+  *pTxt = 0;
+  int rc = SQLITE_OK;
+
+  std::string txt;
+  try {
+    txt.assign( as_smarts ? 
+		RDKit::MolToSmarts(*pCMol, false) : 
+		RDKit::MolToSmiles(*pCMol, true) );
+  } 
+  catch (...) {
+    // unknown exception
+    rc = SQLITE_ERROR;
+  }       
+
+  if (rc == SQLITE_OK) {
+    *pTxt = sqlite3_mprintf("%s", txt.c_str());
+    if (!(*pTxt)) {
+      rc = SQLITE_NOMEM;
+    }
+  }
+
+  return rc;               
+}
+
+// Blob <-> Molecule /////////////////////////////////////////////////////////
+
+int blob_to_cmol(u8 *pBlob, int len, CMol **ppCMol)
+{
+  assert(pBlob);
+  *ppCMol = 0;
+
+  int rc = SQLITE_OK;
+
+  std::string blob;
+  try {
+    blob.assign((const char *)pBlob, len);
+    *ppCMol = new CMol(blob);
+  } 
+  catch (...) {
+    // problem generating molecule from blob data
+    rc = SQLITE_ERROR;
+  }
+  
+  if (!(*ppCMol)) {
+    // blob data could not be parsed
+    rc = SQLITE_ERROR;
+  }
+
+  return rc;
+}
+
+int cmol_to_blob(CMol *pCMol, u8 **ppBlob, int *pLen)
+{
+  assert(pCMol);
+
+  *ppBlob = 0;
+  *pLen = 0;
+
+  int rc = SQLITE_OK;
+  std::string blob;
+  try {
+    RDKit::MolPickler::pickleMol(*pCMol, blob);
+  } 
+  catch (...) {
+    // unknown exception
+    rc = SQLITE_ERROR;
+  }       
+
+  if (rc == SQLITE_OK) {
+    *ppBlob = (u8 *)sqlite3_malloc(blob.size());
+    if (*ppBlob) {
+      memcpy(*ppBlob, blob.data(), blob.size());
+      *pLen = blob.size();
+    }
+    else {
+      rc = SQLITE_NOMEM;
+    }
+  }
+
+  return rc;
+}
+
+// Molecular descriptors /////////////////////////////////////////////////////
 
 double cmol_amw(CMol *pCMol) 
 {
--- rdkit_adapter.h
+++ rdkit_adapter.h
@@ -6,10 +6,17 @@ extern "C" {
 #endif
 
   typedef struct CMol CMol;
+  typedef unsigned char u8;
+  typedef unsigned int u32;
+
 
   void free_cmol(CMol *pCMol);
 
   int txt_to_cmol(const char * txt, bool as_smarts, CMol **ppCMol);
+  int cmol_to_txt(CMol *pCMol, bool as_smarts, char **pTxt);
+
+  int blob_to_cmol(u8 *blob, int len, CMol **ppCMol);
+  int cmol_to_blob(CMol *pCMol, u8 **pBlob, int *pLen);
 
   double cmol_amw(CMol *pCMol);
   double cmol_logp(CMol *pCMol);
