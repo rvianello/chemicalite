5b26f0dd3b7140e4c68ccacc4be2b07bde3edb7f
Riccardo Vianello <riccardo.vianello@gmail.com>
2015-12-08 19:49:57 +0100

    declare SQL functions as SQLITE_DETERMINISTIC

--- utils.h
+++ utils.h
@@ -3,7 +3,7 @@
 
 #define CREATE_SQLITE_FUNCTION(a, func, rc)				\
   if (rc == SQLITE_OK)							\
-    rc = sqlite3_create_function(db, # func, a, SQLITE_UTF8, 0, func##_f, 0, 0)
+    rc = sqlite3_create_function(db, # func, a, SQLITE_UTF8 | SQLITE_DETERMINISTIC, 0, func##_f, 0, 0)
 
 #define CREATE_SQLITE_UNARY_FUNCTION(func, rc)	\
   CREATE_SQLITE_FUNCTION(1, func, rc)
