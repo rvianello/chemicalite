01af56f5befeb8fbb7e9985c81d7004d15d599cf
Riccardo Vianello <riccardo.vianello@gmail.com>
2016-03-24 23:45:06 +0100

    initial database creation

--- examples/example1/main.cpp
+++ examples/example1/main.cpp
@@ -1,3 +1,18 @@
+#include <sqlite3.h>
+
+sqlite3 * create_database(const char * filename)
+{
+  sqlite3 * db;
+  if (sqlite3_open(filename, &db) != SQLITE_OK) {
+  }
+  return db;
+}
+
 int main(int argc, char * argv[])
 {
+  const char * db_path = argv[1];
+  //const char * input_path = argv[2];
+  
+  sqlite3 * db = create_database(db_path);
+  
 }
