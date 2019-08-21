675308e2b970eb49780e55341fe657e30593d17f
Riccardo Vianello <riccardo.vianello@gmail.com>
2019-08-21 22:46:33 +0200

    update

--- conda/apsw/build.sh
+++ conda/apsw/build.sh
@@ -1,4 +1,4 @@
-export CPATH=$PREFIX/include
-python setup.py build --enable=load_extension
-python setup.py install #test
-
+python setup.py \
+    fetch --all --version=3.28.0 \
+    build --enable-all-extensions --enable=load_extension
+python setup.py install --single-version-externally-managed --record=record.txt
