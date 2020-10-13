36898b4be2631e6a8d848b86ecf6d2b043094364
Riccardo Vianello <riccardo.vianello@gmail.com>
2020-10-13 20:09:22 +0000

    remove the (currently) unused conda recipes

--- conda/apsw/build.sh
+++
@@ -1,4 +0,0 @@
-python setup.py \
-    fetch --all --version=3.28.0 \
-    build --enable-all-extensions --enable=load_extension
-python setup.py install --single-version-externally-managed --record=record.txt
