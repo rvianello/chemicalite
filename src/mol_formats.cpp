12d2f99dce77376b15dc34d203a6164f5ea95153
Riccardo Vianello <riccardo.vianello@gmail.com>
2021-02-16 22:15:26 +0000

    fill-in the gaps in the draft mol_to/from_smiles implementation

--- src/mol_formats.cpp
+++ src/mol_formats.cpp
@@ -1,19 +1,54 @@
+#include <cassert>
+#include <string>
+
 #include <sqlite3ext.h>
 extern const sqlite3_api_routines *sqlite3_api;
 
 #include <GraphMol/SmilesParse/SmilesParse.h>
+#include <GraphMol/SmilesParse/SmilesWrite.h>
 
 #include "mol.hpp"
 #include "mol_formats.hpp"
 #include "logging.hpp"
 #include "utils.hpp"
 
-/*static void mol_to_smiles_f(sqlite3_context* ctx, int argc, sqlite3_value** argv)
+static void mol_to_smiles(sqlite3_context* ctx, int argc, sqlite3_value** argv)
 {
-  
-}*/
+  UNUSED(argc);
+  assert(argc == 1);
 
-static void mol_from_smiles_f(sqlite3_context* ctx, int argc, sqlite3_value** argv)
+  sqlite3_value *arg = argv[0];
+  int value_type = sqlite3_value_type(arg);
+
+  /* NULL on NULL */
+  if (value_type == SQLITE_NULL) {
+    sqlite3_result_null(ctx);
+    return;
+  }
+
+  /* not a binary blob */
+  if (value_type != SQLITE_BLOB) {
+    sqlite3_result_error_code(ctx, SQLITE_MISMATCH);
+    chemicalite_log(SQLITE_MISMATCH, "input arg must be of type blob or NULL");
+    return;
+  }
+
+  int rc = SQLITE_OK;
+  std::string blob((const char *)sqlite3_value_blob(arg), sqlite3_value_bytes(arg));
+  RDKit::ROMol * mol = binary_to_romol(blob, &rc);
+
+  if ( rc != SQLITE_OK ) {
+    sqlite3_result_error_code(ctx, rc);
+  }
+  else {
+    std::string smiles = RDKit::MolToSmiles(*mol);
+    sqlite3_result_text(ctx, smiles.c_str(), -1, SQLITE_TRANSIENT);
+  }
+
+  delete mol;
+}
+
+static void mol_from_smiles(sqlite3_context* ctx, int argc, sqlite3_value** argv)
 {
   UNUSED(argc);
   assert(argc == 1);
@@ -35,9 +70,23 @@ static void mol_from_smiles_f(sqlite3_context* ctx, int argc, sqlite3_value** ar
   }
 
   /* build the molecule binary repr from a text string */
-  try {
-    RDKit::ROMol * mol = RDKit::SmilesToMol((const char *)sqlite3_value_text(arg)); 
+  std::string smiles = (const char *)sqlite3_value_text(arg);
+  RDKit::ROMol * mol = nullptr;
 
+  try {
+    mol = RDKit::SmilesToMol(smiles);
+  }
+  catch (...) {
+    chemicalite_log(
+      SQLITE_ERROR,
+      "Conversion from '%s' to mol triggered an exception.",
+      smiles.c_str()
+      );
+    sqlite3_result_null(ctx);
+    return;
+  }
+
+  if (mol) {
     int rc;
     std::string buf = mol_to_binary(mol, &rc);
     if (rc != SQLITE_OK) {
@@ -46,10 +95,14 @@ static void mol_from_smiles_f(sqlite3_context* ctx, int argc, sqlite3_value** ar
     else {
       sqlite3_result_blob(ctx, buf.c_str(), buf.size(), SQLITE_TRANSIENT);
     }
-
     delete mol;
   }
-  catch (...) {
+  else {
+    chemicalite_log(
+      SQLITE_WARNING,
+      "Could not convert '%s' into mol.",
+      smiles.c_str()
+      );
     sqlite3_result_null(ctx);
   }
 }
@@ -59,7 +112,8 @@ int chemicalite_init_mol_formats(sqlite3 *db)
   UNUSED(db);
   int rc = SQLITE_OK;
 
-  CREATE_SQLITE_UNARY_FUNCTION(mol_from_smiles);
+  if (rc == SQLITE_OK) rc = sqlite3_create_function(db, "mol_from_smiles", 1, SQLITE_UTF8 | SQLITE_DETERMINISTIC, 0, mol_from_smiles, 0, 0);
+  if (rc == SQLITE_OK) rc = sqlite3_create_function(db, "mol_to_smiles", 1, SQLITE_UTF8 | SQLITE_DETERMINISTIC, 0, mol_to_smiles, 0, 0);
 
   return rc;
 }
