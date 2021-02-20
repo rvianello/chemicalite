78f6fd9d627729150facddb6ab1a3386380bebd9
Riccardo Vianello <riccardo.vianello@gmail.com>
2021-02-20 16:03:53 +0000

    cleanup

--- src/utils.hpp
+++ src/utils.hpp
@@ -4,15 +4,6 @@
 #define XSTRINGIFY(s) STRINGIFY(s)
 #define STRINGIFY(s) #s
 
-#define CREATE_SQLITE_FUNCTION(a, func)				\
-  if (rc == SQLITE_OK)							\
-    rc = sqlite3_create_function(db, STRINGIFY(func), a, SQLITE_UTF8 | SQLITE_DETERMINISTIC, 0, func##_f, 0, 0)
-
-#define CREATE_SQLITE_NULLARY_FUNCTION(func) CREATE_SQLITE_FUNCTION(0, func)
-#define CREATE_SQLITE_UNARY_FUNCTION(func) CREATE_SQLITE_FUNCTION(1, func)
-#define CREATE_SQLITE_BINARY_FUNCTION(func) CREATE_SQLITE_FUNCTION(2, func)
-#define CREATE_SQLITE_TERNARY_FUNCTION(func) CREATE_SQLITE_FUNCTION(3, func)
-
 template <void (*F)(sqlite3_context*, int, sqlite3_value**)>
 static void strict(sqlite3_context* ctx, int argc, sqlite3_value** argv)
 {
--- src/versions.cpp
+++ src/versions.cpp
@@ -9,22 +9,22 @@ extern const sqlite3_api_routines *sqlite3_api;
 /*
 ** Return the version info for this extension
 */
-static void chemicalite_version_f(sqlite3_context* ctx, int /*argc*/, sqlite3_value** /*argv*/)
+static void chemicalite_version(sqlite3_context* ctx, int /*argc*/, sqlite3_value** /*argv*/)
 {
   sqlite3_result_text(ctx, XSTRINGIFY(CHEMICALITE_VERSION), -1, SQLITE_STATIC);
 }
 
-static void rdkit_version_f(sqlite3_context* ctx, int /*argc*/, sqlite3_value** /*argv*/)
+static void rdkit_version(sqlite3_context* ctx, int /*argc*/, sqlite3_value** /*argv*/)
 {
   sqlite3_result_text(ctx, RDKit::rdkitVersion, -1, SQLITE_STATIC);
 }
 
-static void rdkit_build_f(sqlite3_context* ctx, int /*argc*/, sqlite3_value** /*argv*/)
+static void rdkit_build(sqlite3_context* ctx, int /*argc*/, sqlite3_value** /*argv*/)
 {
   sqlite3_result_text(ctx, RDKit::rdkitBuild, -1, SQLITE_STATIC);
 }
 
-static void boost_version_f(sqlite3_context* ctx, int /*argc*/, sqlite3_value** /*argv*/)
+static void boost_version(sqlite3_context* ctx, int /*argc*/, sqlite3_value** /*argv*/)
 {
   sqlite3_result_text(ctx, RDKit::boostVersion, -1, SQLITE_STATIC);
 }
@@ -32,9 +32,9 @@ static void boost_version_f(sqlite3_context* ctx, int /*argc*/, sqlite3_value** 
 int chemicalite_init_versions(sqlite3 *db)
 {
   int rc = SQLITE_OK;
-  CREATE_SQLITE_NULLARY_FUNCTION(chemicalite_version);
-  CREATE_SQLITE_NULLARY_FUNCTION(rdkit_version);
-  CREATE_SQLITE_NULLARY_FUNCTION(rdkit_build);
-  CREATE_SQLITE_NULLARY_FUNCTION(boost_version);
+  if (rc == SQLITE_OK) rc = sqlite3_create_function(db, "chemicalite_version", 0, SQLITE_UTF8 | SQLITE_DETERMINISTIC, 0, chemicalite_version, 0, 0);
+  if (rc == SQLITE_OK) rc = sqlite3_create_function(db, "rdkit_version", 0, SQLITE_UTF8 | SQLITE_DETERMINISTIC, 0, rdkit_version, 0, 0);
+  if (rc == SQLITE_OK) rc = sqlite3_create_function(db, "rdkit_build", 0, SQLITE_UTF8 | SQLITE_DETERMINISTIC, 0, rdkit_build, 0, 0);
+  if (rc == SQLITE_OK) rc = sqlite3_create_function(db, "boost_version", 0, SQLITE_UTF8 | SQLITE_DETERMINISTIC, 0, boost_version, 0, 0);
   return rc;
 }
