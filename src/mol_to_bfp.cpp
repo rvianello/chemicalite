a40ee7dc78af155045acbbc66df09381746f4a41
Riccardo Vianello <riccardo.vianello@gmail.com>
2021-03-05 22:28:06 +0000

    add morgan bfp

--- src/mol_to_bfp.cpp
+++ src/mol_to_bfp.cpp
@@ -110,6 +110,85 @@ ExplicitBitVect * mol_pattern_bfp(const RDKit::ROMol & mol, int length)
   return RDKit::PatternFingerprintMol(mol, length);
 }
 
+template <ExplicitBitVect * (*F)(const RDKit::ROMol &, int, int), int DEFAULT_LENGTH>
+static void mol_to_morgan_bfp(sqlite3_context* ctx, int argc, sqlite3_value** argv)
+{
+  int rc = SQLITE_OK;
+
+  std::string * pbfp = 0;
+
+  void * aux = sqlite3_get_auxdata(ctx, 0);
+  if (aux) {
+    pbfp = (std::string *) aux;
+  }
+  else {
+    std::unique_ptr<RDKit::ROMol> pmol(arg_to_romol(argv[0], &rc));
+
+    if (rc == SQLITE_OK && sqlite3_value_type(argv[1]) != SQLITE_INTEGER) {
+      rc = SQLITE_MISMATCH;
+    }
+
+    if (rc == SQLITE_OK && argc > 2 && sqlite3_value_type(argv[2]) != SQLITE_INTEGER) {
+      rc = SQLITE_MISMATCH;
+    }
+
+    if (rc == SQLITE_OK && pmol) {
+      int radius = sqlite3_value_int(argv[1]);
+      int length = (argc > 2) ? sqlite3_value_int(argv[2]) : DEFAULT_LENGTH;
+      try {
+        std::unique_ptr<ExplicitBitVect> bv(F(*pmol, radius, length));
+        if (bv) {
+          pbfp = new std::string(BitVectToBinaryText(*bv));
+        }
+        else {
+          rc = SQLITE_ERROR;
+        }
+      } 
+      catch (...) {
+        // unknown exception
+        rc = SQLITE_ERROR;
+      }
+    }
+
+    if (rc == SQLITE_OK) {
+      sqlite3_set_auxdata(ctx, 0, (void *) pbfp, free_bfp_auxdata);
+    }
+  }
+
+  if (rc != SQLITE_OK) {
+    sqlite3_result_error_code(ctx, rc);
+    return;
+  }
+
+  if (!pbfp) {
+    sqlite3_result_null(ctx);
+    return;
+  }
+
+  Blob blob = bfp_to_blob(*pbfp, &rc);
+
+  if (rc == SQLITE_OK) {
+    sqlite3_result_blob(ctx, blob.data(), blob.size(), SQLITE_TRANSIENT);
+  }
+  else {
+    sqlite3_result_error_code(ctx, rc);
+  }
+}
+
+ExplicitBitVect * mol_morgan_bfp(const RDKit::ROMol & mol, int radius, int length)
+{
+  std::vector<uint32_t> invars(mol.getNumAtoms());
+  RDKit::MorganFingerprints::getConnectivityInvariants(mol, invars, true);
+  return RDKit::MorganFingerprints::getFingerprintAsBitVect(mol, radius, length, &invars);
+}
+
+ExplicitBitVect * mol_feat_morgan_bfp(const RDKit::ROMol & mol, int radius, int length)
+{
+  std::vector<uint32_t> invars(mol.getNumAtoms());
+  RDKit::MorganFingerprints::getFeatureInvariants(mol, invars);
+  return RDKit::MorganFingerprints::getFingerprintAsBitVect(mol, radius, length, &invars);
+}
+
 static constexpr const int DEFAULT_SSS_BFP_LENGTH = 2048;
 static constexpr const int DEFAULT_LAYERED_BFP_LENGTH = 1024;
 static constexpr const int DEFAULT_MORGAN_BFP_LENGTH = 512;
@@ -136,6 +215,12 @@ int chemicalite_init_mol_to_bfp(sqlite3 *db)
 
   if (rc == SQLITE_OK) rc = sqlite3_create_function(db, "mol_pattern_bfp", 1, SQLITE_UTF8 | SQLITE_DETERMINISTIC, 0, strict<mol_to_bfp<mol_pattern_bfp, DEFAULT_SSS_BFP_LENGTH>>, 0, 0);
   if (rc == SQLITE_OK) rc = sqlite3_create_function(db, "mol_pattern_bfp", 2, SQLITE_UTF8 | SQLITE_DETERMINISTIC, 0, strict<mol_to_bfp<mol_pattern_bfp, DEFAULT_SSS_BFP_LENGTH>>, 0, 0);
+
+  if (rc == SQLITE_OK) rc = sqlite3_create_function(db, "mol_morgan_bfp", 2, SQLITE_UTF8 | SQLITE_DETERMINISTIC, 0, strict<mol_to_morgan_bfp<mol_morgan_bfp, DEFAULT_MORGAN_BFP_LENGTH>>, 0, 0);
+  if (rc == SQLITE_OK) rc = sqlite3_create_function(db, "mol_morgan_bfp", 3, SQLITE_UTF8 | SQLITE_DETERMINISTIC, 0, strict<mol_to_morgan_bfp<mol_morgan_bfp, DEFAULT_MORGAN_BFP_LENGTH>>, 0, 0);
+
+  if (rc == SQLITE_OK) rc = sqlite3_create_function(db, "mol_feat_morgan_bfp", 2, SQLITE_UTF8 | SQLITE_DETERMINISTIC, 0, strict<mol_to_morgan_bfp<mol_feat_morgan_bfp, DEFAULT_MORGAN_BFP_LENGTH>>, 0, 0);
+  if (rc == SQLITE_OK) rc = sqlite3_create_function(db, "mol_feat_morgan_bfp", 3, SQLITE_UTF8 | SQLITE_DETERMINISTIC, 0, strict<mol_to_morgan_bfp<mol_feat_morgan_bfp, DEFAULT_MORGAN_BFP_LENGTH>>, 0, 0);
 
   return rc;
 }
