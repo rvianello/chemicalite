3e31389aa1c2b3f2198eea933698a422580552e2
Riccardo Vianello <riccardo.vianello@gmail.com>
2021-04-28 19:47:42 +0000

    cleanup

--- src/molecule.h
+++
@@ -1,7 +0,0 @@
-#ifndef CHEMICALITE_MOLECULE_INCLUDED
-#define CHEMICALITE_MOLECULE_INCLUDED
-
-int chemicalite_init_molecule(sqlite3 *db);
-int fetch_mol_arg(sqlite3_value* arg, Mol **ppMol);
-
-#endif
