0d92c0137dc52c70a6a6f7e6cff88e78a655b10c
Riccardo Vianello <riccardo.vianello@gmail.com>
2021-05-15 20:58:54 +0000

    change the sdf_reader into a regular (non-eponymous) virtual table

--- src/sdf_io.cpp
+++ src/sdf_io.cpp
@@ -1,7 +1,9 @@
 #include <cassert>
 #include <cstring>
 #include <fstream>
+#include <iomanip>
 #include <string>
+#include <sstream>
 #include <memory>
 
 #include <GraphMol/FileParsers/MolSupplier.h>
@@ -13,27 +15,32 @@ extern const sqlite3_api_routines *sqlite3_api;
 #include "sdf_io.hpp"
 #include "mol.hpp"
 
-// static const int SDF_MOLECULE_COLUMN = 0;
-static const int SDF_FILEPATH_COLUMN = 1;
+struct SdfReaderVtab : public sqlite3_vtab {
+  std::string filename;
+};
 
-static int sdfReaderConnect(sqlite3 *db, void */*pAux*/,
-                      int /*argc*/, const char * const */*argv*/,
+static int sdfReaderInit(sqlite3 *db, void */*pAux*/,
+                      int argc, const char * const *argv,
                       sqlite3_vtab **ppVTab,
                       char **pzErr)
 {
-  int rc = sqlite3_declare_vtab(db, "CREATE TABLE x("
-    "molecule MOL, "
-    "file_path HIDDEN"
-    ")");
+  if (argc != 4) {
+    return SQLITE_ERROR;
+  }
+
+  std::string filename;
+  std::istringstream filename_ss(argv[3]);
+  filename_ss >> std::quoted(filename);
+
+  int rc = sqlite3_declare_vtab(db, "CREATE TABLE x(molecule MOL)");
 
   if (rc == SQLITE_OK) {
-    sqlite3_vtab *vtab = *ppVTab = (sqlite3_vtab *) sqlite3_malloc(sizeof(sqlite3_vtab));
-    if (!vtab) {
-      rc = SQLITE_NOMEM;
-    }
-    else {
-      memset(vtab, 0, sizeof(sqlite3_vtab));
-    }
+    SdfReaderVtab *vtab = new SdfReaderVtab; // FIXME
+    vtab->nRef = 0;
+    vtab->pModule = 0;
+    vtab->zErrMsg = 0;
+    vtab->filename = filename;
+    *ppVTab = (sqlite3_vtab *) vtab;
   }
 
   if (rc != SQLITE_OK) {
@@ -43,36 +50,33 @@ static int sdfReaderConnect(sqlite3 *db, void */*pAux*/,
   return rc;
 }
 
+static int sdfReaderCreate(sqlite3 *db, void *pAux,
+                      int argc, const char * const *argv,
+                      sqlite3_vtab **ppVTab,
+                      char **pzErr)
+{
+  return sdfReaderInit(db, pAux, argc, argv, ppVTab, pzErr);
+}
+
+static int sdfReaderConnect(sqlite3 *db, void *pAux,
+                      int argc, const char * const *argv,
+                      sqlite3_vtab **ppVTab,
+                      char **pzErr)
+{
+  return sdfReaderInit(db, pAux, argc, argv, ppVTab, pzErr);
+}
+
 int sdfReaderBestIndex(sqlite3_vtab */*pVTab*/, sqlite3_index_info *pIndexInfo)
 {
-  // At this time the only arg passed to the SDF parser is the input file path
-  // this function is consequently very simple. it might become more complex
-  // when/if support for more args/options is implemented. 
-  int file_path_index = -1;
-  for (int index = 0; index < pIndexInfo->nConstraint; ++index) {
-    if (pIndexInfo->aConstraint[index].usable == 0) {
-      continue;
-    }
-    if (pIndexInfo->aConstraint[index].iColumn == SDF_FILEPATH_COLUMN &&
-        pIndexInfo->aConstraint[index].op == SQLITE_INDEX_CONSTRAINT_EQ) {
-      file_path_index = index;
-    }
-  }
-  if (file_path_index < 0) {
-    // The SDF file path is not available, or it's not usable,
-    // This plan is unusable for this table
-    return SQLITE_CONSTRAINT;
-  }
-  pIndexInfo->idxNum = 1; // Not really meaningful a this time
-  pIndexInfo->aConstraintUsage[file_path_index].argvIndex = 1; // will be argv[0] for xFilter
-  pIndexInfo->aConstraintUsage[file_path_index].omit = 1; // no need for SQLite to verify
-  pIndexInfo->estimatedCost = 1000000; 
+  /* A forward scan is the only supported mode, this method is therefore very minimal */
+  pIndexInfo->estimatedCost = 100000; 
   return SQLITE_OK;
 }
 
-static int sdfReaderDisconnect(sqlite3_vtab *pVTab)
+static int sdfReaderDisconnectDestroy(sqlite3_vtab *pVTab)
 {
-  sqlite3_free(pVTab);
+  SdfReaderVtab *vtab = (SdfReaderVtab *)pVTab;
+  delete vtab;
   return SQLITE_OK;
 }
 
@@ -98,22 +102,12 @@ static int sdfReaderClose(sqlite3_vtab_cursor *pCursor)
 }
 
 static int sdfReaderFilter(sqlite3_vtab_cursor *pCursor, int /*idxNum*/, const char */*idxStr*/,
-                     int argc, sqlite3_value **argv)
+                     int /*argc*/, sqlite3_value **/*argv*/)
 {
   SdfReaderCursor *p = (SdfReaderCursor *)pCursor;
+  SdfReaderVtab *vtab = (SdfReaderVtab *)p->pVtab;
 
-  if (argc != 1) {
-    // at present we always expect the input file path as the only arg
-    return SQLITE_ERROR;
-  }
-
-  sqlite3_value *arg = argv[0];
-  if (sqlite3_value_type(arg) != SQLITE_TEXT) {
-    return SQLITE_MISMATCH;
-  }
-
-  const char * file_path = (const char *) sqlite3_value_text(arg);
-  std::unique_ptr<std::ifstream> pins(new std::ifstream(file_path));
+  std::unique_ptr<std::ifstream> pins(new std::ifstream(vtab->filename));
   if (!pins->is_open()) {
     // Maybe log something
     return SQLITE_ERROR;
@@ -184,11 +178,11 @@ static int sdfReaderRowid(sqlite3_vtab_cursor *pCursor, sqlite_int64 *pRowid)
 */
 static sqlite3_module sdfReaderModule = {
   0,                           /* iVersion */
-  0,                           /* xCreate - create a table */ /* null because eponymous-only */
+  sdfReaderCreate,             /* xCreate - create a table */ /* null because eponymous-only */
   sdfReaderConnect,            /* xConnect - connect to an existing table */
   sdfReaderBestIndex,          /* xBestIndex - Determine search strategy */
-  sdfReaderDisconnect,         /* xDisconnect - Disconnect from a table */
-  0,                           /* xDestroy - Drop a table */
+  sdfReaderDisconnectDestroy,  /* xDisconnect - Disconnect from a table */
+  sdfReaderDisconnectDestroy,  /* xDestroy - Drop a table */
   sdfReaderOpen,               /* xOpen - open a cursor */
   sdfReaderClose,              /* xClose - close a cursor */
   sdfReaderFilter,             /* xFilter - configure scan constraints */
