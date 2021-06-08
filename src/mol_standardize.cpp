19df10b1833176eda3f53841bcba28237e9faa34
Riccardo Vianello <riccardo.vianello@gmail.com>
2021-06-08 22:09:01 +0000

    add mol_standardize module (stub)

---
+++ src/mol_standardize.cpp
@@ -0,0 +1,43 @@
+#include <memory>
+
+#include <sqlite3ext.h>
+extern const sqlite3_api_routines *sqlite3_api;
+
+#include <GraphMol/MolStandardize/MolStandardize.h>
+
+#include "utils.hpp"
+#include "mol_standardize.hpp"
+#include "mol.hpp"
+
+template <RDKit::RWMol * (*F)(const RDKit::RWMol &, const RDKit::MolStandardize::CleanupParameters &)>
+static void mol_standardize(sqlite3_context* ctx, int /*argc*/, sqlite3_value** argv)
+{
+  int rc = SQLITE_OK;
+  sqlite3_value *arg = nullptr;
+  
+  // the input molecule
+  arg = argv[0];
+  std::unique_ptr<RDKit::RWMol> mol_in(arg_to_rwmol(arg, &rc));
+  if ( rc != SQLITE_OK ) {
+    sqlite3_result_error_code(ctx, rc);
+    return;
+  }
+
+  std::unique_ptr<RDKit::RWMol> mol_out(F(*mol_in, RDKit::MolStandardize::defaultCleanupParameters));
+
+  Blob blob = mol_to_blob(*mol_out, &rc);
+  if (rc != SQLITE_OK) {
+    sqlite3_result_error_code(ctx, rc);
+  }
+  else {
+    sqlite3_result_blob(ctx, blob.data(), blob.size(), SQLITE_TRANSIENT);
+  }
+}
+
+
+int chemicalite_init_mol_standardize(sqlite3 *db)
+{
+  int rc = SQLITE_OK;
+  if (rc == SQLITE_OK) rc = sqlite3_create_function(db, "mol_cleanup", 1, SQLITE_UTF8 | SQLITE_DETERMINISTIC, 0, mol_standardize<RDKit::MolStandardize::cleanup>, 0, 0);
+  return rc;
+}
---
+++ src/mol_standardize.hpp
@@ -0,0 +1,6 @@
+#ifndef CHEMICALITE_MOL_STANDARDIZE_INCLUDED
+#define CHEMICALITE_MOL_STANDARDIZE_INCLUDED
+
+int chemicalite_init_mol_standardize(sqlite3 *db);
+
+#endif
