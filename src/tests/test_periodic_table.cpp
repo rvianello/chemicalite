73522cd4839eaf997cfa8d795bb063465fd2a374
Riccardo Vianello <riccardo.vianello@gmail.com>
2024-01-18 23:38:58 +0100

    update test on periodic table data following RDKit's #6971

--- src/tests/test_periodic_table.cpp
+++ src/tests/test_periodic_table.cpp
@@ -11,7 +11,7 @@ TEST_CASE("periodic table", "[pte]")
     test_select_value(db, "SELECT symbol FROM periodic_table WHERE atomic_number = 6", "C");
     test_select_value(db, "SELECT atomic_weight FROM periodic_table WHERE atomic_number = 7", 14.007);
     test_select_value(db, "SELECT vdw_radius FROM periodic_table WHERE atomic_number = 8", 1.55);
-    test_select_value(db, "SELECT covalent_radius FROM periodic_table WHERE atomic_number = 9", 0.64);
+    test_select_value(db, "SELECT covalent_radius FROM periodic_table WHERE atomic_number = 9", 0.57);
   }
 
   SECTION("select by symbol")
@@ -19,7 +19,7 @@ TEST_CASE("periodic table", "[pte]")
     test_select_value(db, "SELECT atomic_number FROM periodic_table WHERE symbol = 'C'", 6);
     test_select_value(db, "SELECT atomic_weight FROM periodic_table WHERE symbol = 'N'", 14.007);
     test_select_value(db, "SELECT vdw_radius FROM periodic_table WHERE symbol = 'O'", 1.55);
-    test_select_value(db, "SELECT covalent_radius FROM periodic_table WHERE symbol = 'F'", 0.64);
+    test_select_value(db, "SELECT covalent_radius FROM periodic_table WHERE symbol = 'F'", 0.57);
   }
 
   test_db_close(db);
